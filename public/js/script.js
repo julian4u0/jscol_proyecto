4569391e2e37a222e27f56f1e0f79e0bc3e346e9
Julian Usuga Ortiz <jazulado1337@hotmail.com>
2020-07-20 17:05:45 -0500

    Modo oscuro añadido

--- public/js/script.js
+++ public/js/script.js
@@ -13,3 +13,16 @@ var year = (yy < 1000) ? yy + 1900 : yy;
 var divFecha = document.getElementById("div-fecha");
 divFecha.innerHTML = "Hoy es " + day + " de " + months[month] + " del " + year;
 //document.write("Hoy es " + day + " de " + months[month] + " del " + year);
+
+
+//Modo Oscuro
+function modoOscuro() {
+    var bodyElem = document.body;
+    bodyElem.classList.toggle("dark-mode");
+    var navElem = document.getElementById("navbar");
+    navElem.classList.toggle("dark-mode-nav");
+
+    document.querySelector("#navbar > div > a").classList.toggle("white-text");
+    document.querySelector("#navbarToggler > ul > li:nth-child(1) > a").classList.toggle("white-text-muted");
+    document.querySelector("#navbarToggler > ul > li:nth-child(2) > a").classList.toggle("white-text-muted");
+ }
